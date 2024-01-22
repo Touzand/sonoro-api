000cafae3fe5eb00eae23e5642cbe702c5fafe90
Alan Alonso <alan.alonso.gaf15@gmail.com>
2024-01-22 10:26:27 -0300

    update(middleware): cors

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
